6aa158248b800fdcbbca6b36827ce17811ce72bf
Frederic Bergeron <fbergeron@rocketmail.com>
2021-12-04 01:35:55 +0900

    Complete day 3
    It must be the ugliest solution worldwide.

--- day_03/Day03.java
+++ day_03/Day03.java
@@ -41,26 +41,41 @@ class Day03 {
         return numbers;
     }
 
-    public void run() {
-        ArrayList<String> numbers = readBinaryNumbers("input_short.txt");
-        System.out.print(numbers);
-
+    public int[] getPositiveBits(ArrayList<String> numbers) {
         int[] positiveBits = new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-        int[] negativeBits = new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
         for (int i = 0; i < numbers.size(); i++) {
             String number = numbers.get(i);
             for (int d = 0; d < 12; d++) {
                 char bit = number.charAt(d);
                 if ('1' == bit)
                     positiveBits[d] += 1;
-                else
+            }
+        }
+        return positiveBits;
+    }
+
+    public int[] getNegativeBits(ArrayList<String> numbers) {
+        int[] negativeBits = new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+        for (int i = 0; i < numbers.size(); i++) {
+            String number = numbers.get(i);
+            for (int d = 0; d < 12; d++) {
+                char bit = number.charAt(d);
+                if ('0' == bit)
                     negativeBits[d] += 1;
             }
         }
+        return negativeBits;
+    }
+
+
+    public void run() {
+        ArrayList<String> numbers = readBinaryNumbers("input.txt");
+        System.out.print(numbers);
+
+        int[] positiveBits = getPositiveBits(numbers);
+        int[] negativeBits = getNegativeBits(numbers);
         StringBuffer strGammaRate = new StringBuffer();
         StringBuffer strEpsilonRate = new StringBuffer();
-        ListArray<String> oxigenGeneratorRate = new ListArray<String>();
-        ListArray<String> co2ScrubbingRate = new ListArray<String>();
         for (int d = 0; d < 12; d++) {
             if (positiveBits[d] > negativeBits[d]) {
                 strGammaRate.append("1");
@@ -71,6 +86,13 @@ class Day03 {
                 strEpsilonRate.append("1");
             }
         }
+        System.out.println("positiveBits");
+        for (int d = 0; d < 12; d++)
+            System.out.println(positiveBits[d]);
+        System.out.println("negativeBits");
+        for (int d = 0; d < 12; d++)
+            System.out.println(negativeBits[d]);
+        System.out.println("negativeBits="+negativeBits);
         System.out.println("strGammaRate="+strGammaRate);
         System.out.println("strEpsilonRate="+strEpsilonRate);
         int gammaRate = Integer.parseInt(strGammaRate.toString(), 2);
@@ -78,8 +100,63 @@ class Day03 {
         System.out.println(gammaRate);
         System.out.println(epsilonRate);
         int powerConsumption = gammaRate * epsilonRate;
-        System.out.println("powerConsumption="+powerConsumptiong);
+        System.out.println("powerConsumption="+powerConsumption);
 
+        ArrayList<String> oxigenGeneratorRate = new ArrayList<String>();
+        oxigenGeneratorRate.addAll(numbers);
+        ArrayList<String> co2ScrubbingRate = new ArrayList<String>();
+        co2ScrubbingRate.addAll(numbers);
+        System.out.println("oxigenGeneratorRate="+oxigenGeneratorRate);
+        System.out.println("co2ScrubbingRate="+co2ScrubbingRate);
+        for (int d = 0; d < 12; d++) {
+            int[] positiveBitsOxigen = getPositiveBits(oxigenGeneratorRate);
+            int[] negativeBitsOxigen = getNegativeBits(oxigenGeneratorRate);
+
+            int[] positiveBitsCo2 = getPositiveBits(co2ScrubbingRate);
+            int[] negativeBitsCo2 = getNegativeBits(co2ScrubbingRate);
+
+            char oxigenGeneratorBitCriteria = positiveBitsOxigen[d] >= negativeBitsOxigen[d] ? '1' : '0';
+            char co2ScrubbingBitCriteria = negativeBitsCo2[d] <= positiveBitsCo2[d] ? '0' : '1';
+            System.out.println("d="+d);
+            System.out.println("oxigenGeneratorBitCriteria="+oxigenGeneratorBitCriteria);
+            System.out.println("co2ScrubbingBitCriteria="+co2ScrubbingBitCriteria);
+            if (oxigenGeneratorRate.size() > 1) {
+                ArrayList<String> numbersToRemove = new ArrayList<String>();
+                for (int i = 0; i < oxigenGeneratorRate.size(); i++) {
+                    String number = oxigenGeneratorRate.get(i);
+                    char bit = number.charAt(d);
+                    if (bit != oxigenGeneratorBitCriteria) {
+                        numbersToRemove.add(number);
+                        System.out.println("remove " + number + " for oxigenGeneratorRate");
+                    }
+                }
+                for (int i = 0; i < numbersToRemove.size(); i++) {
+                    oxigenGeneratorRate.remove(numbersToRemove.get(i));
+                }
+            }
+            if (co2ScrubbingRate.size() > 1) {
+                ArrayList<String> numbersToRemove = new ArrayList<String>();
+                for (int i = 0; i < co2ScrubbingRate.size(); i++) {
+                    String number = co2ScrubbingRate.get(i);
+                    char bit = number.charAt(d);
+                    if (bit != co2ScrubbingBitCriteria) {
+                        numbersToRemove.add(number);
+                        System.out.println("remove " + number + " for co2ScrubbingRate");
+                    }
+                }
+                for (int i = 0; i < numbersToRemove.size(); i++) {
+                    co2ScrubbingRate.remove(numbersToRemove.get(i));
+                }
+            }
+        }
+        System.out.println("oxigenGeneratorRate="+oxigenGeneratorRate);
+        System.out.println("co2ScrubbingRate="+co2ScrubbingRate);
+        int oxigenGenRate = Integer.parseInt(oxigenGeneratorRate.get(0), 2);
+        int co2ScrubRate = Integer.parseInt(co2ScrubbingRate.get(0), 2);
+        System.out.println("oxigenGenRate="+oxigenGenRate);
+        System.out.println("co2ScrubRate="+co2ScrubRate);
+        int lifeSupportRate = oxigenGenRate * co2ScrubRate;
+        System.out.println("lifeSupportRate="+lifeSupportRate);
     }
 
     public static void main(String[] args) {
